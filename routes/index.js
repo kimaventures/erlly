fc6678da9c9a015e9c16aed67446a47c8730d068
felixfelix <felix@doctrine.fr>
2016-09-08 20:59:08 +0200

    fixed bug ./start.sh

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
 
-router.get('/test', function(req, res, next){
-	res.render('test')
-})
 
-module.exports = router;
+	router.get('/', function(req, res, next){
+		res.render('onboarding/home');
+	})
+
+
+// always at the end
+	module.exports = router;
