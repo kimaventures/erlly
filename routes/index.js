13ca5c5238c6e43d99ab79254ff46ba8e12c95cd
felixfelix <felix@doctrine.fr>
2016-09-09 10:51:26 +0200

    sass base

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+//onboarding pages
+router.get('/', function(req, res, next){ res.render('onboarding/home'); })
 
-
-router.get('/', function(req, res, next){
-	res.render('onboarding/home');
-})
-
+router.get('/team', function(req, res, next){ res.render('static/team'); })
 
 // always at the end
 	module.exports = router;
